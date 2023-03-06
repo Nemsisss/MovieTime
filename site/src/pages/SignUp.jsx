a92073266f5ecd6a0b1ac964778a66f284285116
Angela Steinmetz <awsteinm@usc.edu>
2023-03-05 22:23:06 -0800

    commit edits to sign up page

--- site/src/pages/SignUp.jsx
+++ site/src/pages/SignUp.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import NestedComponent from "../components/NestedComponent";
-
-// Use a nested component with provided props to redirect the user to "/"
-// and a counter which is modified by a set of buttons
+// import { useNavigate } from "react-router-dom";
+import "../styles/SignUp.css"
 function SignUp() {
     // States for registration
     const [email, setEmail] = useState('');
@@ -11,7 +9,12 @@ function SignUp() {
 
     // States for checking the errors
     const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState(false);
+    const [errorEmailEmp, setError] = useState(false);
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorPass, setErrorPass] = useState(false);
+    const [errorPassVal, setErrorPassVal] = useState(false);
+    const [errorPassValEmp, setErrorPassCheck] = useState(false);
+    const [errorPassEmp, setErrorPassEmpt] = useState(false);
 
     // Handling the email change
     const handleEmail = (e) => {
@@ -31,24 +34,61 @@ function SignUp() {
         setSubmitted(false);
     };
 
+    const switchPage = (e) => {
+        console.log("switched")
+    }
+
+    // const switchPage = (e) => {
+    //     let path = 'newpath';
+    //     navigate(path);
+    // }
+
     // Handling the form submission
     const handleSubmit = (e) => {
         e.preventDefault();
+        setError(false);
+        setErrorPassVal(false);
+        setErrorPass(false);
+        setErrorPassCheck(false);
+        setErrorPassVal(false);
+        setErrorPassEmpt(false);
+        setErrorEmail(false);
         let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         let testPass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{4,}$/;
-        if(!re.test(email)){
-            setError(true);
-        } else if (passwordCheck === '' || email === '' || password === '') {
-            setError(true);
-        } else if(password !== passwordCheck){
-            setError(true);
-        } else if (!testPass.test(password)){
+        let passed = true;
+        if (passwordCheck === '') {
+            passed = false;
+            setErrorPassCheck(true);
+        }
+        if (email === ''){
+            passed = false;
             setError(true);
         }
-        else {
-            setSubmitted(true);
-            setError(false);
+        if (password === ''){
+            passed = false;
+            setErrorPassEmpt(true)
         }
+        if(passed){
+            if(!re.test(email)) {
+                setErrorEmail(true);
+            }
+            else if(password !== passwordCheck){
+                setErrorPass(true);
+            } else if (!testPass.test(password)){
+                setErrorPassVal(true);
+            }
+            else{
+                setSubmitted(true);
+            }
+        }
+
+        // } else if (passwordCheck === '') {
+        //     setErrorPassCheck(true);
+        // } else if (email === ''){
+        //     setError(true);
+        // } else if (password === ''){
+        //     setErrorPassEmpt(true)
+        // }
     };
 
     // Showing success message
@@ -59,55 +99,125 @@ function SignUp() {
                 style={{
                     display: submitted ? '' : 'none',
                 }}>
-                <h1>User {name} successfully registered!!</h1>
+                User {name} successfully registered!!
             </div>
         );
     };
 
 // Showing error message if error is true
-    const errorMessageEmpty = () => {
+    const errorMessageEmptyEmail = () => {
         return (
             <div
                 className="error"
                 style={{
-                    display: error ? '' : 'none',
+                    display: errorEmailEmp ? '' : 'none',
                 }}>
-                <h1>Please enter all the fields</h1>
+                Field cannot be empty
+            </div>
+        );
+    };
+
+    const errorMessageInvalidEmail= () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: errorEmail ? '' : 'none',
+                }}>
+                Please enter a valid email
+            </div>
+        );
+    };
+
+    const errorMessagePass= () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: errorPass ? '' : 'none',
+                }}>
+                Please ensure passwords match
+            </div>
+        );
+    };
+
+    const errorMessagePassEmp= () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: errorPassEmp ? '' : 'none',
+                }}>
+                Field cannot be empty
+            </div>
+        );
+    };
+
+    const errorMessagePassVal= () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: errorPassVal ? '' : 'none',
+                }}>
+                Please ensure you enter a valid password
+            </div>
+        );
+    };
+
+    const errorMessagePassValEmp= () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: errorPassValEmp ? '' : 'none',
+                }}>
+                Field cannot be empty
             </div>
         );
     };
 
     return (
-        <div className="form">
-            <div>
-                <h1 className="centerAlign">User Registration Page</h1>
+        <div className="app">
+            <div className="login-form">
+                <div className="title">User Registration Page</div>
+                <div className="messages">
+                    {successMessage()}
+                </div>
+                <div className="form">
+                    <form className="formStyle">
+                        {/* Labels and inputs for form data */}
+                        <div className="input-container">
+                            <label className="label" htmlFor="email">Email</label>
+                            <input onChange={handleEmail} className="input"
+                                   value={email} type="email" id="email"/>
+                            {errorMessageEmptyEmail()}
+                            {errorMessageInvalidEmail()}
+                        </div>
+
+                        <div className="input-container">
+                            <label className="label" htmlFor="password">Password</label>
+                            <input onChange={handlePassword} className="input"
+                                   value={password} type="password" id="password"/>
+                            {errorMessagePassVal()}
+                            {errorMessagePassEmp()}
+                        </div>
+
+                        <div className="input-container">
+                            <label className="label" htmlFor="passwordCheck">Confirm Password</label>
+                            <input onChange={handlePasswordCheck} className="input"
+                                   value={passwordCheck} type="password" id="passwordCheck"/>
+                            {errorMessagePass()}
+                            {errorMessagePassValEmp()}
+                        </div>
+
+                        <button onClick={handleSubmit} className="btn" type="submit" data-testid="submit-button">
+                                Submit
+                        </button>
+                    </form>
+                </div>
+                <div className="centerAlign redirect" onClick={switchPage}>Have an account? Login Here</div>
             </div>
-
-            {/* Calling to the methods */}
-            <div className="messages">
-                {errorMessageEmpty()}
-                {successMessage()}
-            </div>
-
-            <form className="formStyle">
-                {/* Labels and inputs for form data */}
-
-                <label className="label" htmlFor="email">Email</label>
-                <input onChange={handleEmail} className="input"
-                       value={email} type="email" id="email"/>
-
-                <label className="label" htmlFor="password">Password</label>
-                <input onChange={handlePassword} className="input"
-                       value={password} type="password" id="password"/>
-
-                <label className="label" htmlFor="passwordCheck">Confirm Password</label>
-                <input onChange={handlePasswordCheck} className="input"
-                       value={passwordCheck} type="password" id="passwordCheck"/>
-
-                <button onClick={handleSubmit} className="btn" type="submit" data-testid="submit-button">
-                    Submit
-                </button>
-            </form>
         </div>
     );
 }
