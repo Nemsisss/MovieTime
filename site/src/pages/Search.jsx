fcaf8646726394f20b8edd127416162b83f68e31
Angela Steinmetz <awsteinm@usc.edu>
2023-03-09 13:58:52 -0800

    fix issues

--- site/src/pages/Search.jsx
+++ site/src/pages/Search.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import "../styles/search.css";
 // import Navbar from "../components/Navbar";
 import httpRequest from "../utils/httpRequest";
--- site/src/utils/httpRequest.jsx
+++ site/src/utils/httpRequest.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import React from "react";
-import { act } from "react-dom/test-utils";
+// import React from "react";
+// import { act } from "react-dom/test-utils";
 import "regenerator-runtime/runtime.js";
 
 
