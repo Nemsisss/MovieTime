10f68d6f2e2f547ebf4d406d95d8bd6635421b63
Sohee Yoon <yoonshlove@gmail.com>
2023-03-09 12:57:04 -0800

    Update App.js

--- site/src/App.js
+++ site/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Other from "./pages/Other";
 import "bootstrap/dist/css/bootstrap.min.css"
 import LoginForm from "./LoginForm"
 import "./styles/login.css"
@@ -12,11 +10,10 @@ function App() {
     <div>
       <Routes>
         {/* Root pages, located in /pages/ */}
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/other" element={<Other />} />
         <Route path="/login" element={<LoginForm />} />
         {/* 404 page not found redirect */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+
       </Routes>
     </div>
   );
