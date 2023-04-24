963d67618ede5c001fe8569c75f1dc9d8b926c8a
Angela Steinmetz <awsteinm@usc.edu>
2023-04-24 15:53:42 -0700

    linking pages, fixing stuff, so fun

--- src/test/java/edu/usc/csci310/project/SpringBootAPITest.java
+++ src/test/java/edu/usc/csci310/project/SpringBootAPITest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SpringBootAPITest {
-
     @Autowired
     private MockMvc mockMvc;
 
